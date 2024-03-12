516700a9f371ca97b5d588cb8cf19e0a34e8e362
raiyanabrar-enosis <raiyan.abrar@enosisbd.com>
2024-03-12 14:28:39 +0600

    Code refactor

--- app/index.ts
+++ app/index.ts
@@ -1,91 +1,41 @@
 import { Location, User, Company } from "./types/types";
+import { faker } from "@faker-js/faker";
 
 // @ts-ignore
 import { setupMap } from "./map.ts";
 
-const userNames = [
-	"AlexSmith",
-	"LilyJones",
-	"JohnDoe",
-	"EmilyJohnson",
-	"MichaelBrown",
-	"SarahWilson",
-	"DavidMiller",
-	"JessicaDavis",
-	"MatthewTaylor",
-	"OliviaMartinez",
-];
-
-const companyNames = [
-	"TechNova Solutions",
-	"Horizon Innovations",
-	"Summit Enterprises",
-	"Quantum Dynamics",
-	"FusionWorks Inc.",
-	"Nexus Technologies",
-	"Stellar Ventures",
-	"Apex Solutions Group",
-	"Pinnacle Industries",
-	"Elevation Enterprises",
-];
-
-const catchPhrases = [
-	"Innovating Tomorrow, Today!",
-	"Where Dreams Meet Technology.",
-	"Inspiring Excellence, Every Step of the Way.",
-	"Empowering Your Future, Today.",
-	"Building Bridges to Success.",
-	"Transforming Ideas into Reality.",
-	"Your Success, Our Passion.",
-	"Leading the Way in Innovation.",
-	"Elevating Expectations, Every Time.",
-	"Driving Progress, Together.",
-];
-
-const randomlyGenerateUsers = (size: number): User[] => {
-	let users = new Array<User>();
-
-	for (let index = 0; index < size; index++) {
-		const userIndex = Math.floor(Math.random() * userNames.length);
-
-		const user = {
-			name: userNames[userIndex],
-			location: {
-				lattitude: Math.random() * (100 - -100) + -100,
-				longitude: Math.random() * (180 - -180) + -180,
-			},
-		} as User;
-
-		users.push(user);
-	}
-
-	return users;
+const generateLocation = (): Location => {
+	return {
+		lattitude: faker.location.latitude(),
+		longitude: faker.location.longitude(),
+	};
 };
 
-const randomlyGenerateCompany = (): Company => {
-	const companyIndex = Math.floor(Math.random() * companyNames.length);
-	const catchPhraseIndex = Math.floor(Math.random() * catchPhrases.length);
+const randomlyGenerateItem = <Type>(type: string): Type => {
+	// if type is user, set the name to person.fullName, else company.name
+	// add the key catchPhrase if type is company
+	// location key is common for both types
+	const item = {
+		name: type === "user" ? faker.person.fullName() : faker.company.name(),
+		location: generateLocation(),
+		...(type === "company" && { catchPhrase: faker.company.catchPhrase() }),
+	} as Type;
 
-	const company = {
-		name: companyNames[companyIndex],
-		location: {
-			lattitude: Math.random() * (100 - -100) + -100,
-			longitude: Math.random() * (180 - -180) + -180,
-		},
-		catchPhrase: catchPhrases[catchPhraseIndex],
-	} as Company;
-
-	return company;
+	return item;
 };
 
 const companySize = 8;
+const userSize = 8;
 const companies = new Array<Company>();
+const users = new Array<User>();
 
 for (let company = 0; company < companySize; company++) {
-	companies.push(randomlyGenerateCompany());
+	companies.push(randomlyGenerateItem<Company>("company"));
 }
 
-const users = randomlyGenerateUsers(13);
+for (let user = 0; user < userSize; user++) {
+	users.push(randomlyGenerateItem<User>("user"));
+}
 
 console.log(companies);
 console.log(users);
